c5f944fe8d2df4ad0b09e79b95275f145d666951
Tun1n <slpedro21@outlook.com>
2023-09-04 19:50:32 -0300

    Exercícios márcios

---
+++ JurosSimples.py
@@ -0,0 +1,7 @@
+capital = 1000
+taxa_juros = 5
+tempo = 12
+
+juros = (capital * taxa_juros * tempo) / 100
+
+print(f"O valor do juros acumulado em 12 meses é: {juros}")
---
+++ ÁreaTriângulo.py
@@ -0,0 +1,6 @@
+largura = 10
+altura = 4
+
+area = largura * altura
+
+print(f"A área do triângulo é: {area}")
